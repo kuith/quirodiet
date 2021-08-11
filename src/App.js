f18a9385f12ba4cb29b4e39c1978c11398cf3c14
kuith <kuithsteam@gmail.com>
2021-08-11 20:51:37 +0200

    primer commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="container">
+      Buenas
+    </div>
+  );
+}
+
+export default App;
---
+++ src/parametros/precios.js
@@ -0,0 +1,10 @@
+export const quiromasaje = {
+    quiromasajeCard: 22,
+    quiromasajeDeportivo: 30,
+    quiromasajeRelajante: 30,
+    quiromasajeCirculatorio: 30,
+    quiromasajeDescontracturante: 30,
+    quiromasajeRecuperador: 30,
+    quiromasajeDeportistas: 25,
+    quiromasajeJubilados: 30
+}
