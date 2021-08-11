adb220ff447dd0c83b457e7a66160b263f9048ca
kuith <kuithsteam@gmail.com>
2021-08-11 21:03:13 +0200

    precios subidos

--- src/parametros/precios.js
+++ src/parametros/precios.js
@@ -7,5 +7,42 @@ export const quiromasaje = {
     quiromasajeRecuperador: 30,
     quiromasajeDeportistas: 25,
     quiromasajeJubilados: 30,
-    pruebas:33
+}
+
+export const osteopatia = {
+    osteopatiaCard: 22
+}
+
+export const presoterapia = {
+    presoterapiaCard: 22,
+    presoterapiaSesion: 30
+}
+
+export const asesoramientoDeportivo = {
+    asesoramientoDeportivoCard: 20,
+    asesoramientoDeportivoVolumen: 20,
+    asesoramientoDeportivoTonificacion: 20
+}
+
+export const asesoramientoDietetico = {
+    asesoramientoDieteticoCard: 20,
+    asesoramientoDieteticoPrimera: 25,
+    asesoramientoDieteticoSeguimiento: 20
+}
+
+export const bonos = {
+    bonosCard: 76,
+    bonoMasajes3: 76,
+    bonoMasajes6: 147,
+    bonoMasajes10: 220,
+    bonoAnticelulitico: 160,
+    bonoDieta: 35
+}
+
+export const rejuvenecimiento = {
+    rejuvenecimientoCard: 60
+}
+
+export const eliminacionTatuajes = {
+    eliminacionTayuajesCard: "60 / 80"
 }
