b885e00c945b4493c6f23481c724e6b3aa1df308
kuith <kuithsteam@gmail.com>
2021-10-24 21:28:16 +0200

    arreglado nomre de clase en k¡logo.js

--- src/vistas/logo.js
+++ src/vistas/logo.js
@@ -1,12 +1,11 @@
 import React from 'react';
-//import Image from 'react-bootstrap/Image';
 
 import LogoPrincipal from '../utils/images/logprincipal_trans.png';
 
 const Logo = () => {
   return (
     <div>
-      <img src={LogoPrincipal} class="img-fluid mx-auto d-block LogoPrincipal" alt="Logo"></img>
+      <img src={LogoPrincipal} className="img-fluid mx-auto d-block LogoPrincipal" alt="Logo"></img>
     </div>
   );
 }
